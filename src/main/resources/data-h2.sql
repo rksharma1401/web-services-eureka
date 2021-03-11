d53061552ba1ce0edc8c7a2d9fb8e4a0419b7603
Ravikant Sharma <ravikant.sharma@fisglobal.com>
2021-03-11 18:14:26 +0530

    remove insert on startup

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,4 @@
 --INSERT INTO person(first_name, last_name, age, place) VALUES('Avi','Sharma',2,'Indore');
 --INSERT INTO person(first_name, last_name, age, place) VALUES('Ravikant','Sharma',30,'Indore');
 --INSERT INTO person(first_name, last_name, age, place) VALUES('Triyaksh','Sharma',3,'Indore');
+select * from person;
